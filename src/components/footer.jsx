5b42da2131bd9b1ec74ef5bdfba6350620158de1
techdeus <sullivan.marlon@gmail.com>
2019-12-27 08:35:29 -0800

    Configured Footer component
    Added "a" links with new tab opener
    Added "a" hover styling

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,7 +4,15 @@ import '../stylesheets/components/footer.scss';
 function Footer() {
     return (
         <div className="footerWrapper">
-            <span>Powered by TechDeus</span>
+            <div className="lowerFooter">
+                <a href="https://www.laboulangeriesf.com/" target="_blank">
+                    <span>Laboulangerie Cafe</span>
+                </a>
+                <a href="https://www.linkedin.com/in/techdeus/" target="_blank">
+                    <span>Powered by TechDeus</span>
+                </a>
+                <span>Copyright &copy; 2019</span>
+            </div>
         </div>
     )
 }
