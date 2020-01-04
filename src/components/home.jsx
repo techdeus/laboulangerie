3c3896080b006966b83d73ed8a21ba07558aad0f
techdeus <sullivan.marlon@gmail.com>
2020-01-04 02:17:18 -0800

    Create Sort Capabilities for User
    User can sort products alphabetically
    User can sort products by unique categories

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -5,13 +5,21 @@ import Product from './product';
 import Loader from './loader';
 import Axios from 'axios';
 import '../stylesheets/base/home.scss';
+import Radio from '@material-ui/core/Radio';
+import NativeSelect from '@material-ui/core/NativeSelect';
+import InputLabel from '@material-ui/core/InputLabel';
+import Divider from '@material-ui/core/Divider';
 
 function Home() {
     const [ products, setProducts ] = useState([]);
+    const [ masterProducts, setMasterProducts ] = useState([]);
     const [ loading, setLoading ] = useState(false);
     const [ error, setError ] = useState('');
+    const [selectedValue, setSelectedValue] = useState('asc');
+    const [categories, setCategories] = useState([]);
+    const [category, setCategory] = useState('');
     const { appInfo, cart } = useContext(InfoContext);
-    
+
     if (appInfo[0]['message'] !== "Logged In") {
         return <Redirect to='/' />;
     }
@@ -24,13 +32,40 @@ function Home() {
         })
             .then((res) => {
                 const data = res.data;
-                const sortedData = data.sort((a, b) => a.name > b.name ? 1 : -1);
+                
+                const categories = data.map((product) => {
+                    return product.category;
+                });
+                
+                const uniqueCategories = [...new Set(categories)].sort((a, b) => a > b ? 1 : -1);
+                
+                const sortedData = selectedValue === 'asc' ? data.sort((a, b) => a.name > b.name ? 1 : -1) : data.sort((a, b) => a.name < b.name ? 1 : -1) ;
+                setCategories(uniqueCategories);
                 setProducts(sortedData);
+                setMasterProducts(data);
                 setLoading(false);
             })
                 .catch(err => setError(err.response.data.message));
-    }, []);
+    }, [selectedValue]);
     
+    const handleChange = event => {
+        setSelectedValue(event.target.value);
+    }
+
+    const selectCategory = event => {
+        event.preventDefault();
+        const currCategory = event.target.value;
+        const filterProducts = masterProducts.filter((product) => {
+            return product.category === currCategory;
+        });
+        setCategory(currCategory);
+        setProducts(filterProducts);
+        return;
+    }
+
+    const reset = () => {
+        window.location.reload(true);
+    };
 
     return (
         <div className="homeWrapper">
@@ -40,6 +75,51 @@ function Home() {
             {
                 error ? <div>{error} </div> : null
             }
+            <div className="displayWrapper">
+                <div className="sortWrapper">
+                    <InputLabel htmlFor="radio-button">A-Z</InputLabel>
+                    <Radio 
+                        checked={selectedValue === 'asc'}
+                        onChange={handleChange}
+                        value="asc"
+                        name="radio-button"
+                        inputProps={{ 'aria-label': 'Ascending'}}
+                    />
+                    <InputLabel htmlFor="radio-button">Z-A</InputLabel>
+                    <Radio 
+                        checked={selectedValue === 'desc'}
+                        onChange={handleChange}
+                        value="desc"
+                        name="radio-button"
+                        inputProps={{ 'aria-label': 'Descending'}}
+                    />
+                </div>
+                <Divider className="dividerIcon" orientation="vertical" />
+                <div className="selectWrapper">
+                    <InputLabel htmlFor="category-native-helper">Category</InputLabel>
+                        <NativeSelect
+                            className="nativeSelect"
+                            value={category}
+                            onChange={selectCategory}
+                            inputProps={{
+                                name: 'category',
+                                id: 'category-native-helper',
+                                className: 'nativeSelect',
+                            }}
+                        >
+                            <option value="" />
+                            {
+                                categories.map((category, index) => (
+                                    <option className="nativeSelect" key={index} value={category}>{category}</option>
+                                ))
+                            }
+                        </NativeSelect>
+                    </div>
+                <Divider className="dividerIcon" orientation="vertical" />
+                <div className="resetIcon">
+                    <span className="resetText" onClick={() => reset()}>reset</span>
+                </div>
+            </div>
             <div className="productsWrapper">
                 {
                     products.length > 0 ? 
