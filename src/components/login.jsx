aad336788f8ee4ba5d3829319fea378225ce0a81
techdeus <sullivan.marlon@gmail.com>
2020-01-06 17:55:39 -0800

    Change message

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -126,7 +126,7 @@ function Login() {
                 }
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 318"><path fill="#e2e1e0" fillOpacity="1" d="M0,64L48,53.3C96,43,192,21,288,48C384,75,480,149,576,176C672,203,768,181,864,160C960,139,1056,117,1152,112C1248,107,1344,117,1392,122.7L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
                 </form>
-                <div className="forgotPassword" role="button" onClick={showForgotPassword}>Did you forget password?</div>
+                <div className="forgotPassword" role="button" onClick={showForgotPassword}>Did you forget the password?</div>
                 {
                     showForgot ? 
                     <div className="forgotWrapper">
