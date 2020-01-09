dc05315b68780bb32bf518cfb60e331a6038df82
techdeus <sullivan.marlon@gmail.com>
2020-01-09 01:15:13 -0800

    Created a dashboard landing page.
    Allow the user to decide where to land upon logging in.
    See designs.
    Links to products and orders page.

---
+++ src/components/dashboard.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
+import { useHistory } from 'react-router-dom';
+import Loader from './loader';
+import '../stylesheets/base/dashboard.scss';
+
+function Dashboard() {
+    const [loading, setLoading] = useState(true);
+    const [items, setItems] = useState(['products', 'orders']);
+    const [orders, setOrders] = useState([]);
+    const [products, setProducts] = useState([]);
+    const [error, setError] = useState('');
+    const history = useHistory();
+
+    useEffect(() => {
+        Axios.get('/dashboard')
+            .then((res) => {
+                setOrders(res.data.orders);
+                setProducts(res.data.products);
+                setLoading(false);
+            })
+                .catch(err => setError(err.data.response.message));
+
+    }, []);
+
+    const goLink = (url) => {
+        return history.push(`/${url}`);
+    };
+
+    return (
+        <div className="dashboardWrapper">
+            {
+                items.map((item, index) => (
+                    <div key={index} className="dashitemWrapper" role="button" onClick={() => goLink(`${item}`)}>
+                        <div className="dash-name">{item}</div>
+                        <div className="dash-metric">{item.length}</div>
+                        <div className="dash-circle"><img className="dash-image" src={`/img/assets/dash-${item}.svg`} /></div>
+                        <div className="dash-view">view</div>
+                    </div>
+                ))
+            }
+            {
+                loading ? <Loader isLoading={loading} size="4rem" thickness={2} /> : null
+            }
+            {
+                error ? <div className="error">{error}</div> : null
+            }
+        </div>
+    );
+}
+
+export default Dashboard;
--- src/components/routes.jsx
+++ src/components/routes.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from 'react';
 import { InfoContext } from './store';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import Dashboard from './dashboard';
 import Home from './home';
 import Login from './login';
 import Error from './error';
@@ -22,7 +23,8 @@ function Routes() {
                 }
                 <Switch>
                     <Route exact path="/" component={Login} />
-                    <Route exact path="/home" component={Home} />
+                    <Route exact path="/home" component={Dashboard} />
+                    <Route exact path="/products" component={Home} />
                     <Route exact path="/passwordchange" component={ChangePassword} />
                     <Route component={Error} />
                 </Switch>
