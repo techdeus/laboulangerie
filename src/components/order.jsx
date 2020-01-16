9147a341826127c1123899a12582999f7840b30b
techdeus <sullivan.marlon@gmail.com>
2020-01-15 16:39:34 -0800

    Added Order setup. SCSS File

---
+++ src/components/order.jsx
@@ -0,0 +1,10 @@
+import React, { useContext, useEffect } from 'react';
+import '../stylesheets/components/orders.scss';
+
+function Order() {
+    return (
+        <div>You have a Order</div>
+    )
+}
+
+export default Order;
---
+++ src/components/orders.jsx
@@ -0,0 +1,13 @@
+import React, { useContext, useEffect } from 'react';
+import '../stylesheets/components/orders.scss';
+import Order from './order';
+
+function Orders() {
+    return (
+        <div>Hello Mr.
+            <Order />
+        </div>
+    );
+}
+
+export default Orders;
